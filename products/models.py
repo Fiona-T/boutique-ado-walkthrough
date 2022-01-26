2347e8cb01956c80d97d937fc39d00b1476b7dc4
Fiona-T <fitracey@gmail.com>
2022-01-26 15:41:19 +0000

    feat: create products app, models, admin, fixtures and migrate

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Category model"""
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """string method, return category name"""
+        return self.name
+
+    def get_friendly_name(self):
+        """return category friendly name for views"""
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """Product model"""
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+        )
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+        )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """string method, return product name"""
+        return self.name
