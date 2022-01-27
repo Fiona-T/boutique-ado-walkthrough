901b496278489181a1e9c25c9d07f3148a912fa3
Fiona-T <fitracey@gmail.com>
2022-01-27 12:59:20 +0000

    feat: add product sorting by price, rating, category in main nav and all products view

--- products/views.py
+++ products/views.py
@@ -8,12 +8,37 @@ def all_products(request):
     """view to show all products, and sorting and searching """
     # all products - these are displayed if there is no search or filter
     products = Product.objects.all()
+    # below are all set to None initially. To make sure they are defined when returned
+    # to the template when there is no query/categories/sorting etc.
     # search query - set to None initially
     query = None
     # category filter - set to None initially
     categories = None
+    # search query - set to None initially
+    sort = None
+    # category filter - set to None initially
+    direction = None
 
     if request.GET:
+        # if sort is in the get request (contained in url)
+        if 'sort' in request.GET:
+            # set this variable to sort in the get request
+            sortkey = request.GET['sort']
+            # preserve the orignal field we want to sort on, which is name
+            sort = sortkey
+            # to allow case insensitive sorting
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            # if direction is also in the get url
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    # reverse the order if descending in get request
+                    sortkey = f'-{sortkey}'
+            # actually sort the products
+            products = products.order_by(sortkey)
+
         # if category is in the get request (contained in url)
         if 'category' in request.GET:
             # split into a list at the commas
@@ -33,11 +58,15 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             # filter the products to the ones matching the search
             products = products.filter(queries)
+    # how the products are currently sorted - sorting by price/rating/category and asc/desc
+    # this will be returned to the template so can use it in the template
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
