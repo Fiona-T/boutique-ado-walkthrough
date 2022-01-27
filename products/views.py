aacad689e4a3893df65b32dd7938f12ab15e489b
Fiona-T <fitracey@gmail.com>
2022-01-27 15:22:12 +0000

    feat: add selected categories, search results and product home link to products page, and add product category to product detao;s

--- products/views.py
+++ products/views.py
@@ -30,6 +30,10 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                # add on the name of the category (from related field)
+                # so that when sorted below, it is sorted by category name
+                sortkey = 'category__name'
             # if direction is also in the get url
             if 'direction' in request.GET:
                 direction = request.GET['direction']
