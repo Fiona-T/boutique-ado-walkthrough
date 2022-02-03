d7997a346a7aa8fa17e850de026fa4b2b4935910
Fiona-T <fitracey@gmail.com>
2022-02-03 17:37:27 +0000

    feat: add stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,33 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+// get the text from the script elements, and stlice off the quotation marks
+const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+const client_secret = $('#id_client_secret').text().slice(1, -1);
+// stripe public key
+const stripe = Stripe(stripe_public_key);
+// create instance of stripe elements
+const elements = stripe.elements();
+const style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+// create a card element (in stripe docs referred to as paymentElement)
+const card = elements.create('card', {style: style});
+// mount the card element to the div in the checkout.html file
+card.mount('#card-element');
