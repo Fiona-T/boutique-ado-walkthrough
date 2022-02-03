1076198acc8747be359b4600e07cbbdfd1eb8cbd
Fiona-T <fitracey@gmail.com>
2022-02-03 15:17:40 +0000

    feat: add checkout template, view, url, install crispy forms

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    # get bag from session
+    bag = request.session.get('bag', {})
+    # if nothing in bag, error message and redirect to products page
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+    # empty instance of orderform
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
