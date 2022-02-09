d900601fb318ac105757b854e4683e7a088a0bc4
Fiona-T <fitracey@gmail.com>
2022-02-09 14:31:30 +0000

    add delete view and url, update products and product detail with edit+delete links

--- products/views.py
+++ products/views.py
@@ -94,9 +94,10 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         # if form valid, success msg and redirect
         if form.is_valid():
-            form.save()
+            # store the product, so can pass the id back in the redirect
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request, 'Failed to add product. Please ensure the form is valid.'
@@ -144,3 +145,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """admin user delete a product"""
+    # get the product passed in via url
+    product = get_object_or_404(Product, pk=product_id)
+    # delete the product
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
