d0be09868e43650423db0839ccf20113d4261b5d
Fiona-T <fitracey@gmail.com>
2022-02-09 14:42:05 +0000

    add login required decorator and check if superuser

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .forms import ProductForm
@@ -87,8 +88,15 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """admin user add product to store"""
+    # if they are not a superuser, redirect them to home page with msg
+    if not request.user.is_superuser:
+        messages.error(
+                request, 'Sorry, only store owners can do that.'
+                )
+        return redirect(reverse('home'))
     if request.method == 'POST':
         # instantiate form from request.POST and request.FILES for upload file
         form = ProductForm(request.POST, request.FILES)
@@ -114,8 +122,15 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """admin user edit a product"""
+    # if they are not a superuser, redirect them to home page with msg
+    if not request.user.is_superuser:
+        messages.error(
+                request, 'Sorry, only store owners can do that.'
+                )
+        return redirect(reverse('home'))
     # get the product passed in via url
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -147,8 +162,15 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """admin user delete a product"""
+    # if they are not a superuser, redirect them to home page with msg
+    if not request.user.is_superuser:
+        messages.error(
+                request, 'Sorry, only store owners can do that.'
+                )
+        return redirect(reverse('home'))
     # get the product passed in via url
     product = get_object_or_404(Product, pk=product_id)
     # delete the product
