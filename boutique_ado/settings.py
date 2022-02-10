8acaa2fcd5fd74217c3b2e0f2152b7c20ce60427
Fiona-T <fitracey@gmail.com>
2022-02-10 16:15:12 +0000

    remove insecure secret key, add environment secret key in settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+cer!)(4^bq9rb!4@&1k#wz&47jj56$^c)_)f0+)p4l3^cb#vc'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-walkthrough.herokuapp.com', 'localhost']
 
