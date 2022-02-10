4f9f7f7f1e8c470640855fd7990af50465e1ef00
Fiona-T <fitracey@gmail.com>
2022-02-10 16:00:10 +0000

    fix typo in database settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABSE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     # DATABASE URL is in Heroku config var - so if there, use the HerokuPostgres
     DATABASES = {
             'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
